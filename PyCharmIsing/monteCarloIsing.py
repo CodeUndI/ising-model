57f9a1ea1336d4c5a098bdd2494fa1ada8cfc99b
Wojtek <wojciech.dwornik98@gmail.com>
2022-05-27 22:47:17 +0200

    pojedynczy spin i krok MCS

--- PyCharmIsing/monteCarloIsing.py
+++ PyCharmIsing/monteCarloIsing.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+import singleFlipDynamic as sf
 
-x = np.ones((10,10))    #tworzy tablice 10x10 wypełnioną 1
+x = np.ones((50,50))    #tworzy tablice 10x10 wypełnioną 1
 
-x[1,2:5]=-1
 print(x)
 
 def saveData(data,name):
@@ -13,3 +13,7 @@ def saveData(data,name):
     df = pd.DataFrame(data=data.astype(float))
     df = df.astype(int)
     df.to_csv(name, sep=' ', header=False, index=False)
+
+for i in range(10000):
+    sf.MCSFlip(x,len(x),2.26)
+print(x)
--- PyCharmIsing/singleFlipDynamic.py
+++ PyCharmIsing/singleFlipDynamic.py
@@ -1,1 +1,35 @@
-#
+import random
+import math
+
+#Pojedyncza aktualizacja uładu
+
+def MCSFlip(data,L,T):
+    """Sekewencja zmiany spinów dla 1 kroku Monte Carlo"""
+    for i in range(L**2):
+        singleFlip(data, L, T)
+
+def singleFlip(data, L, T):
+    """Sekwencyjna zmiana pojedynczego spinu
+    data - siatka spinów
+    L - długość siatki !musi być poprawna!"""
+    i, j = math.floor(random.random()*L), math.floor(random.random()*L)
+    E = 0
+    if i == L-1 or j ==L-1:
+
+        if i == L-1 and j != L-1:
+            E = 2*data[i, j]*(data[i-1, j] + data[0, j] + data[i, j-1] + data[i, j+1])
+
+        elif i != L-1 and j == L-1:
+            E = 2 * data[i, j] * (data[i - 1, j] + data[i + 1, j] + data[i, j - 1] + data[i, 0])
+
+        else:
+            E = 2 * data[i, j] * (data[i - 1, j] + data[0, j] + data[i, j - 1] + data[i, 0])
+
+    else: E = 2*data[i, j]*(data[i-1, j] + data[i+1, j] + data[i, j-1] + data[i, j+1])
+
+    if E <= 0:
+        data[i, j] = -data[i, j]
+    else:
+        x = random.random()
+        if x < math.exp(-E/T):
+            data[i, j] = -data[i, j]
